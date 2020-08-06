6899dc1862f1a14945cbf213832b64d74c74c7e2
Prasan Pani <prasannajeet89@gmail.com>
2020-08-06 12:53:06 -0400

    Added Kotlin @Parcelize annotation

--- app/src/main/java/com/prasan/kotlinmvvmhiltflowapp/data/datamodel/PhotoDetails.kt
+++ app/src/main/java/com/prasan/kotlinmvvmhiltflowapp/data/datamodel/PhotoDetails.kt
@@ -1,8 +1,9 @@
 package com.prasan.kotlinmvvmhiltflowapp.data.datamodel
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class PhotoDetails(
     val url: String?,
     val description: String?,
@@ -15,42 +16,4 @@ data class PhotoDetails(
     val userPhotoUrl: String?,
     val exif: String?,
     val durationPosted: String?
-) : Parcelable {
-    constructor(source: Parcel) : this(
-        source.readString(),
-        source.readString(),
-        source.readValue(Int::class.java.classLoader) as Int?,
-        source.readValue(Int::class.java.classLoader) as Int?,
-        source.readValue(Double::class.java.classLoader) as Double?,
-        source.readValue(Int::class.java.classLoader) as Int?,
-        source.readString(),
-        source.readString(),
-        source.readString(),
-        source.readString(),
-        source.readString()
-    )
-
-    override fun describeContents() = 0
-
-    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
-        writeString(url)
-        writeString(description)
-        writeValue(votesCount)
-        writeValue(comments)
-        writeValue(pulse)
-        writeValue(impressions)
-        writeString(photoTitle)
-        writeString(userName)
-        writeString(userPhotoUrl)
-        writeString(exif)
-        writeString(durationPosted)
-    }
-
-    companion object {
-        @JvmField
-        val CREATOR: Parcelable.Creator<PhotoDetails> = object : Parcelable.Creator<PhotoDetails> {
-            override fun createFromParcel(source: Parcel): PhotoDetails = PhotoDetails(source)
-            override fun newArray(size: Int): Array<PhotoDetails?> = arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
